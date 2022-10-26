098a17079e403b8c19b680abdf4b2ea40594fda0
Elshen01 <mrnagizade1@gmail.com>
2022-10-26 09:11:51 +0400

    3 text elave etdim

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("MyFirstProject");
         System.out.println("MySocndProject");
+        System.out.println("HelloWorld");
     }
 }
